467784fd415774549ad11cc781e83de84343e3d7
Bonifase Orwa <bonifaseorwa@Bonifase-MacBook-Pro.local>
2020-04-17 09:59:06 +0300

    add home and workout components and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { WorkoutsComponent } from './workouts/workouts.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {path: 'workouts', component: WorkoutsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
